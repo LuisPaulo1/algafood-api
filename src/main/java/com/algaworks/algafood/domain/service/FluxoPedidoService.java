90b17dc4021defac58bbe5102e225341ecc38a60
LuisPaulo1 <csluispaulo@gmail.com>
2021-06-10 11:29:21 -0300

    15.4. Usando o serviço de envio de e-mails na confirmação de pedidos

--- src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.service.EnvioEmailService.Mensagem;
 
 @Service
 public class FluxoPedidoService {
@@ -12,10 +13,22 @@ public class FluxoPedidoService {
 	@Autowired
 	private EmissaoPedidoService emissaoPedidoService;
 	
+	@Autowired
+	private EnvioEmailService envioEmailService;
+	
 	@Transactional
 	public void confirmar(String codigo) {
 		Pedido pedido = emissaoPedidoService.buscar(codigo);
 		pedido.confirmar();
+		
+		var mensagem = Mensagem.builder()
+				.assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
+				.corpo("O pedido de código <strong>" 
+						+ pedido.getCodigo() + "</strong> foi confirmado!")
+				.destinatario(pedido.getCliente().getEmail())
+				.build();
+		
+		envioEmailService.enviar(mensagem);
 	}
 	
 	@Transactional
