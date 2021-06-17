d510741358179efa7f32c0021b81315eba0989f8
LuisPaulo1 <csluispaulo@gmail.com>
2021-06-17 10:58:40 -0300

    18.12. Descrevendo códigos de status de respostas de forma global

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -1,15 +1,23 @@
 package com.algaworks.algafood.core.openapi;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -25,16 +33,12 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 				.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
 				.build()
 				.apiInfo(apiInfo())
-				.tags(tags()[0], tags());		
-	}
+				.tags(tags()[0], tags())		
+				.useDefaultResponseMessages(false)
+				.globalResponses(HttpMethod.GET, globalGetResponseMessages());
+	}	
 	
-	private Tag[] tags() {
-	    return new Tag[] {
-	    	new Tag("Cidades", "Gerencia as cidades")	        
-	    };
-	}
-	
-	public ApiInfo apiInfo() {
+	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
 				.title("AlgaFood API")
 				.description("API aberta para clientes e restaurantes")
@@ -42,6 +46,25 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 				.contact(new Contact("AlgaWorks", "https://www.algaworks.com", "contato@algaworks.com"))
 				.build();
 	}
+	
+	private Tag[] tags() {
+		return new Tag[] {
+				new Tag("Cidades", "Gerencia as cidades")	        
+		};
+	}
+	
+	private List<Response> globalGetResponseMessages() {
+		return Arrays.asList(
+			new ResponseBuilder()
+				.code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+				.description("Erro interno do servidor")
+				.build(),
+			new ResponseBuilder()					
+				.code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+				.description("Recurso não possui representação que poderia ser aceita pelo consumidor")
+				.build()
+		);			
+	}	
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
