fa1cce92653cfca0815a2b06545175c5bda87f80
LuisPaulo1 <csluispaulo@gmail.com>
2021-06-20 11:21:47 -0300

    18.34. Desafio: descrevendo documentação de endpoints de produtos

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -93,7 +93,8 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 				new Tag("Cozinhas", "Gerencia as cozinhas"),
 				new Tag("Formas de pagamento", "Gerencia as formas de pagamento"),
 				new Tag("Restaurantes", "Gerencia os restaurantes"),
-				new Tag("Estados", "Gerencia os estados")
+				new Tag("Estados", "Gerencia os estados"),
+				new Tag("Produtos", "Gerencia os produtos de restaurantes")
 		};
 	}
 	
