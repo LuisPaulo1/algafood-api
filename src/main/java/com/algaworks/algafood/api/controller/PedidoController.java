d9bea9a188af2003b1c66fde84126cc9bdce48b6
LuisPaulo1 <csluispaulo@gmail.com>
2021-06-30 18:33:35 -0300

    19.17. Corrigindo links de paginação com ordenação

--- src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
@@ -27,6 +27,7 @@ import com.algaworks.algafood.api.model.PedidoModel;
 import com.algaworks.algafood.api.model.PedidoResumoModel;
 import com.algaworks.algafood.api.model.input.PedidoInput;
 import com.algaworks.algafood.api.openapi.controller.PedidoControllerOpenApi;
+import com.algaworks.algafood.core.data.PageWrapper;
 import com.algaworks.algafood.core.data.PageableTranslator;
 import com.algaworks.algafood.domain.filter.PedidoFilter;
 import com.algaworks.algafood.domain.model.Pedido;
@@ -54,8 +55,9 @@ public class PedidoController implements PedidoControllerOpenApi {
 	
 	@GetMapping
 	public ResponseEntity<PagedModel<PedidoResumoModel>> pesquisar(PedidoFilter filtro, @PageableDefault(size = 10) Pageable pageable){	
-		pageable = traduzirPageable(pageable);		
-		Page<Pedido> pedidosPage = emisaoPedidoService.pesquisar(filtro, pageable);			
+		Pageable pageableTraduzido = traduzirPageable(pageable);		
+		Page<Pedido> pedidosPage = emisaoPedidoService.pesquisar(filtro, pageableTraduzido);	
+		pedidosPage = new PageWrapper<>(pedidosPage, pageable);
 		PagedModel<PedidoResumoModel> pedidosResumoPagedModel = pagedResourcesAssembler.toModel(pedidosPage, pedidoResumoModelAssembler);		
 		return ResponseEntity.ok(pedidosResumoPagedModel);
 	}
