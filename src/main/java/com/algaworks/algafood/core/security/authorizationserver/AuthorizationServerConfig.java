280d7471643a4985dfe75bd8c216a4624c6989f9
LuisPaulo1 <csluispaulo@gmail.com>
2021-07-21 16:01:55 -0300

    24.21. Resolvendo problemas com storage de Authorization Codes

--- src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
@@ -20,6 +20,7 @@ import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
 import org.springframework.security.oauth2.provider.approval.ApprovalStore;
 import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
@@ -67,6 +68,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		endpoints
 			.authenticationManager(authenticationManager)
 			.userDetailsService(userDetailsService)
+			.authorizationCodeServices(new JdbcAuthorizationCodeServices(dataSource))
 			.reuseRefreshTokens(false)	
 			.accessTokenConverter(jwtAccessTokenConverter())
 			.tokenEnhancer(enhancerChain)
@@ -103,7 +105,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		var keyStorePass = jwtKeyStoreProperties.getPassword();
 		var keyPairAlias = jwtKeyStoreProperties.getKeypairAlias();
 		
-		var keyStoreKeyFactory = new KeyStoreKeyFactory(jwtKeyStoreProperties.getJksLocation(), keyStorePass.toCharArray());
+		var keyStoreKeyFactory = new KeyStoreKeyFactory(
+				jwtKeyStoreProperties.getJksLocation(), keyStorePass.toCharArray());
 		
 		return keyStoreKeyFactory.getKeyPair(keyPairAlias);
 	}
--- src/main/java/com/algaworks/algafood/core/web/WebConfig.java
+++ src/main/java/com/algaworks/algafood/core/web/WebConfig.java
@@ -5,6 +5,7 @@ import javax.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,4 +16,8 @@ public class WebConfig implements WebMvcConfigurer {
 		return new ShallowEtagHeaderFilter();
 	}	
 	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("*");
+	}
 }
